a2c6f212c6f88c229f292e5994e3cbfc2610c8ac
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-06-13 20:22:14 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Cart from './pages/cart'
-import Header from './components/Header';
+import './App.css';
+import LandingPage from './Stores/Pages/LandingPage';
+import Mobile from './Stores/Pages/Mobile';
+import AcPage from './Stores/Pages/AcPage';
+import BooksPage from './Stores/Pages/BooksPage';
+import ComputerPage from './Stores/Pages/ComputerPage';
+import FridgePage from './Stores/Pages/FridgePage';
+import KitchenPage from './Stores/Pages/KitchenPage';
+import FurniturePage from './Stores/Pages/FurniturePage';
+import MensPage from './Stores/Pages/MensPage';
+import SpeakerPage from './Stores/Pages/SpeakerPage';
+import TvPage from './Stores/Pages/TvPage';
+import WatchPage from './Stores/Pages/WatchPage';
+import LadysPages from './Stores/Pages/LadysPages';
+import MobileSingle from './Singles/MobileSingle';
 
 function App() {
   return (
     <div>
-     <Header/>
-      <Routes>
-       <Route exact path='/' element={<Home/>} />
-        <Route exact path='/cart' element={<Cart/>} />
-      </Routes>
+
+    <Routes>
+      <Route path='/' element={ <LandingPage/>}/>
+      <Route path='/mobiles' element={<Mobile/>}/>
+      <Route path='/ac' element={<AcPage/>}/>
+      <Route path='/books' element={<BooksPage/>}/>
+      <Route path='/laptops' element={<ComputerPage/>}/>
+      <Route path='/fridge' element={<FridgePage/>}/>
+      <Route path='/kitchen' element={<KitchenPage/>}/>
+      <Route path='/furniture' element={<FurniturePage/>}/>
+      <Route path='/men' element={<MensPage/>}/>
+      <Route path='/speakers' element={<SpeakerPage/>}/>
+      <Route path='/tv' element={<TvPage/>}/>
+      <Route path='/watch' element={<WatchPage/>}/>
+      <Route path='/ladys' element={<LadysPages/>}/>
+         <Route path='/mobiles/:id' element={<MobileSingle/>}/>
+    </Routes>
     </div>
   );
 }
