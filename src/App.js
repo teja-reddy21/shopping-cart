262bc538a35f170763812b32cf8c57a9e06e8661
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-06-16 21:22:44 +0530

    added dynamic content

--- src/App.js
+++ src/App.js
@@ -15,6 +15,17 @@ import TvPage from './Stores/Pages/TvPage';
 import WatchPage from './Stores/Pages/WatchPage';
 import LadysPages from './Stores/Pages/LadysPages';
 import MobileSingle from './Singles/MobileSingle';
+import AcSinglePage from './Singles/AcSinglePage';
+import ComputerSingle from './Singles/ComputerSingle';
+import BooksSingle from './Singles/BooksSingle';
+import FridgeSingle from './Singles/FridgeSingle';
+import FurnitureSingle from './Singles/FurnitureSingle';
+import KitchenSingle from './Singles/KitchenSingle';
+import TvSingelPage from './Singles/TvSingelPage';
+import WatchSinglePage from './Singles/WatchSinglePage';
+import LadysSingle from './Singles/LadysSingle';
+import MenSinglePage from './Singles/MenSinglePage';
+import SpeakerSingle from './Singles/SpeakerSingle';
 
 function App() {
   return (
@@ -35,6 +46,17 @@ function App() {
       <Route path='/watch' element={<WatchPage/>}/>
       <Route path='/ladys' element={<LadysPages/>}/>
          <Route path='/mobiles/:id' element={<MobileSingle/>}/>
+         <Route path='/ac/:id' element={<AcSinglePage/>}/>
+         <Route path='/laptops/:id' element={<ComputerSingle/>}/>
+        <Route path='/books/:id' element={<BooksSingle/>}/>
+         <Route path='/fridge/:id' element={<FridgeSingle/>}/>
+         <Route path='/furniture/:id' element={<FurnitureSingle/>}/>
+         <Route path='/kitchen/:id' element={<KitchenSingle/>}/>
+         <Route path='/tv/:id' element={<TvSingelPage/>}/>
+         <Route path='/watch/:id' element={<WatchSinglePage/>}/>
+         <Route path='/ladys/:id' element={<LadysSingle/>}/>
+         <Route path='/mens/:id' element={<MenSinglePage/>}/>
+         <Route path='/speakers/:id' element={<SpeakerSingle/>}/>
     </Routes>
     </div>
   );
